d19ce2b496db0ed8da817759d481d98edbabde4d
sungyun <csu1940@gmail.com>
2025-05-14 17:27:57 +0900

    AOP추가

--- src/main/java/sungyun/hello_spring/service/SpringConfig.java
+++ src/main/java/sungyun/hello_spring/service/SpringConfig.java
@@ -1,16 +1,11 @@
 package sungyun.hello_spring.service;
 
 
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sungyun.hello_spring.repository.JdbcTemplateMemberRepository;
-import sungyun.hello_spring.repository.JpaMemberRepository;
+import sungyun.hello_spring.aop.TimeTraceAop;
 import sungyun.hello_spring.repository.MemberRepository;
-import sungyun.hello_spring.repository.MemoryMemberRepository;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -42,6 +37,13 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+    /*
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }
+    */
 
     //@Bean
     //public MemberRepository memberRepository() {
