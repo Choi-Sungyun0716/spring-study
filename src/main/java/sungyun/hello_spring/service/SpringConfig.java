db4e881f4a3b662f473a572e326f355d2ae9d5c5
sungyun <csu1940@gmail.com>
2025-05-14 16:45:38 +0900

    spring data jpa 추가

--- src/main/java/sungyun/hello_spring/service/SpringConfig.java
+++ src/main/java/sungyun/hello_spring/service/SpringConfig.java
@@ -21,23 +21,33 @@ public class SpringConfig {
     //    this.dataSource = dataSource;
     // }
 
-    private final EntityManager em;
+
+
+    /*
+        private final EntityManager em;
+
+        @Autowired
+        public SpringConfig(EntityManager em) {
+            this.em = em;
+        }
+        */
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+    //@Bean
+    //public MemberRepository memberRepository() {
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    //}
 
 
 
